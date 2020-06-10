7226cca69d8f6a2cd735779d5fe599afb60066e8
liuyi <liuxy0551@qq.com>
2020-06-10 21:10:48 +0800

    可以发送十六进制了,也可以接收数据了

--- write.js
+++ write.js
@@ -1,7 +1,7 @@
 let SerialPort = require('serialport')
 
 // 设置串口号，波特率，关闭自动开启
-var port = new SerialPort('COM3', {
+var port = new SerialPort('/dev/tty.usbserial-14440', {
   baudRate: 9600,
   dataBits: 8,        // 数据位
   parity: 'none',     // 奇偶校验
@@ -20,8 +20,17 @@ port.open(function (err) {
     // 接受串口数据，并打印到终端
     port.on('data', function (data) {
       console.log('数据接收: ' + data)
+       //收hex
+    console.log('recv: ' + data.toString('hex'));
+    //收字符串
+    console.log('recv: ' + data.toString('ascii'));
     })
-    port.write('0x55 0x56 0x00 0x00 0x00 0x01 0x01 0xAD', function (err) {
+
+    // let data = [0x55, 0x56, 0x00, 0x00, 0x00, 0x01, 0x01, 0xAD]
+    // let data = [0x55, 0x56, 0x00, 0x00, 0x00, 0x01, 0x02, 0xAE]
+    let data = [0x55, 0x56, 0x00, 0x00, 0x00, 0x00, 0x00, 0xAB]
+    port.write(data, function (err) {
+      console.log(1, err)
       if (err) {
         return console.log('Error on write: ' + err.message)
       }
