d6ee96d3ea8c97c3f988cc479b795066ca586212
liuyi <liuxy0551@qq.com>
2020-06-11 10:10:44 +0800

    update: 整理方法

---
+++ README.md
@@ -0,0 +1,11 @@
+https://github.com/TLScottChen/node-serialport-example
+
+https://github.com/node-serialport/node-serialport/blob/4.0.1/README.md
+
+https://www.cnblogs.com/TLScott/p/10358534.html
+
+
+#### 1、查看串口列表
+```
+node list.js
+```
--- write.js
+++ write.js
@@ -1,40 +1,33 @@
-let SerialPort = require('serialport')
+const SerialPort = require('serialport')
 
 // 设置串口号，波特率，关闭自动开启
-var port = new SerialPort('/dev/tty.usbserial-14440', {
+const port = new SerialPort('/dev/tty.usbserial-14440', {
   baudRate: 9600,
-  dataBits: 8,        // 数据位
-  parity: 'none',     // 奇偶校验
-  stopBits: 1,        // 停止位
-  // flowControl: false,
+  dataBits: 8, // 数据位
+  parity: 'none', // 无校验
+  stopBits: 1, // 停止位
   autoOpen: false
 })
 
-// console.log(port)
 // 串口打开
 port.open(function (err) {
   if (err) {
     return console.log('打开失败: ', err.message)
   } else {
-    console.log('打开成功')
-    // 接受串口数据，并打印到终端
     port.on('data', function (data) {
-      console.log('数据接收: ' + data)
-       //收hex
-    console.log('recv: ' + data.toString('hex'));
-    //收字符串
-    console.log('recv: ' + data.toString('ascii'));
+      console.log('数据接收：' + data.toString('hex'))
+      let str = data.toString('hex')
+      console.log(str.indexOf('02'))
     })
 
-    // let data = [0x55, 0x56, 0x00, 0x00, 0x00, 0x01, 0x01, 0xAD]
-    // let data = [0x55, 0x56, 0x00, 0x00, 0x00, 0x01, 0x02, 0xAE]
-    let data = [0x55, 0x56, 0x00, 0x00, 0x00, 0x00, 0x00, 0xAB]
-    port.write(data, function (err) {
-      console.log(1, err)
+    // 发送 hex
+    let data1 = [0x55, 0x56, 0x00, 0x00, 0x00, 0x01, 0x01, 0xAD]  // 开
+    let data2 = [0x55, 0x56, 0x00, 0x00, 0x00, 0x01, 0x02, 0xAE]  // 关
+    let data3 = [0x55, 0x56, 0x00, 0x00, 0x00, 0x00, 0x00, 0xAB] // 查状态
+    port.write(data3, function (err) {
       if (err) {
-        return console.log('Error on write: ' + err.message)
+        return console.log('写入失败: ' + err.message)
       }
-      console.log('message written')
     })
   }
 })
